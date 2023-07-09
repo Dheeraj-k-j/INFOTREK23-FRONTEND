45a50bd7ef768380493a69dc97e02b70a15c4b71
Abhiusnees <abhiusnees.live36@gmail.com>
2023-07-10 01:17:01 +0530

    Events Page Added

---
+++ src/components/Modal.jsx
@@ -0,0 +1,26 @@
+import { Children } from "react";
+import { useEffect } from "react";
+import ReactDOM from "react-dom";
+import '../styles/modal.css';
+
+const Modal = ({ closeModal, children, handleCloseButton }) => {
+  useEffect(() => {
+    document.body.style.overflowY = "hidden";
+    return () => {
+      document.body.style.overflowY = "scroll";
+    };
+  }, []);
+
+  return ReactDOM.createPortal(
+    <>
+      <div className="modal-wrapper" onClick={closeModal}></div>
+      <div className="modal-container">
+        {children}
+        {handleCloseButton}
+      </div>
+    </>,
+    document.querySelector("#modal-root")
+  );
+};
+
+export default Modal;
