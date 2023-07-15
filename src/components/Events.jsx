9f3d69e5012aa286cd81e051087dff8b1a46cf37
Abhiusnees <abhiusnees.live36@gmail.com>
2023-07-15 11:28:06 +0530

    modified events

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,65 +1,21 @@
-import React from "react";
-import EventCard from "./EventCard";
+import React from 'react'
+import EventCard from './EventCard';
 import eventsData from "../constants/eventsData";
-import "../styles/events.css";
 
 const Events = () => {
-  return(
+  return (
     <>
-    <div className="event-cards-page">
-      <h1 className="page-title">EVENTS</h1>
-      <div className="event-cards-container">
+    <div className='events-page'>
+        <h1 className='page-title'>Events</h1>
+        <div className='events-card-container'>
         {
           eventsData.map((event) => (
             <EventCard key={event.id} event={event} />
           ))}
-      </div>
+        </div>
     </div>
     </>
-  );
-};
+  )
+}
 
 export default Events;
-
-
-
-
-
-
-
-
-
-
-// import React from 'react'
-// import EventCard from './EventCard'
-
-// function Events() {
-//   // Later will be fetrched from database
-//   const events = [
-//     {
-//       id:"1",
-//       eventUrl: "https://www.mythrillfiction.com/the-dark-rider",
-//       altName: "Mythrill",
-//       coverImageurl : "https://ggayane.github.io/css-experiments/cards/dark_rider-cover.jpg",
-//       titleImageurl : "https://ggayane.github.io/css-experiments/cards/dark_rider-title.png",
-//       characterImageurl : "https://ggayane.github.io/css-experiments/cards/dark_rider-title.png"
-//     },
-//     {
-//       id:"1",
-//       eventUrl: "https://www.mythrillfiction.com/the-dark-rider",
-//       altName: "Mythrill",
-//       coverImageurl : "https://ggayane.github.io/css-experiments/cards/dark_rider-cover.jpg",
-//       titleImageurl : "https://ggayane.github.io/css-experiments/cards/dark_rider-title.png",
-//       characterImageurl : "https://ggayane.github.io/css-experiments/cards/dark_rider-title.png"
-//     }
-//   ]
-//   return (
-//     <div className="eventContainer">
-//       {events.map((ele)=>(
-//         <EventCard ele={ele}/>
-//       ))}
-//     </div>
-//   )
-// }
-
-// export default Events
